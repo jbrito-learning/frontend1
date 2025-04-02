75a7ab0472b4cd55a2d2b05225e3dbedc168ce2c
jbrito <joaodcbrito7@gmail.com>
2025-04-02 19:44:38 +0100

    Remove obsolete README-aulas.md and README-projeto.md files

---
+++ aulas/types.js
@@ -0,0 +1,43 @@
+const post = {
+  id: 1,
+  title: "Post 1",
+  body: "Body 1",
+  author: "John Doe",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const postList = [post];
+
+const navmenu = [
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Articles",
+    url: "/articles",
+  },
+  {
+    id: 3,
+    name: "About",
+    url: "/about",
+  },
+];
+
+const tags = [
+  {
+    id: 1,
+    name: "Tag 1",
+  },
+  {
+    id: 2,
+    name: "Tag 2",
+  },
+  {
+    id: 3,
+    name: "Tag 3",
+  },
+];
